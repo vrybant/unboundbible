33f8814adf30fad14a43cec05ecebab2891646c7
Vladimir Rybant <vrybant@gmail.com>
2019-09-06 22:46:14 +0300

    ids

--- unitdata.pas
+++ unitdata.pas
@@ -146,14 +146,15 @@ var
   Options : TCopyOptions;
 
 const
-  MaxBooks = 79;
+  MaxBooks = 88;
 
 var
   myBibleArray : array [1..MaxBooks] of integer = (
     010,020,030,040,050,060,070,080,090,100,110,120,130,140,150,160,190,220,230,240,
     250,260,290,300,310,330,340,350,360,370,380,390,400,410,420,430,440,450,460,470,
     480,490,500,510,520,530,540,550,560,570,580,590,600,610,620,630,640,650,660,670,
-    680,690,700,710,720,730,165,170,180,270,280,315,320,462,464,466,468,790,467
+    680,690,700,710,720,730,000,000,000,000,000,000,000,000,000,000,165,468,170,180,
+    462,464,466,467,270,280,315,320
     );
 
   BibleHubArray : array [1..66] of string = (
@@ -167,8 +168,6 @@ var
     );
 
 function IsNewTestament(n: integer): boolean;
-function IsOldTestament(n: integer): boolean;
-function IsApocrypha(n: integer): boolean;
 
 procedure CreateDataDirectory;
 function ConfigFile: string;
@@ -179,17 +178,7 @@ implementation
 
 function IsNewTestament(n: integer): boolean;
 begin
-  Result := (n >= 40) and (n <= 66);
-end;
-
-function IsOldTestament(n: integer): boolean;
-begin
-  Result := n < 40;
-end;
-
-function IsApocrypha(n: integer): boolean;
-begin
-  Result := n > 66;
+  Result := (n >= 40) and (n < 77);
 end;
 
 function DataPath: string;
