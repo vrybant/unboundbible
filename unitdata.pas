6f5153f0b063bd099757f0cebe9ef69edf5e6bdd
Vladimir Rybant <vrybant@gmail.com>
2020-06-04 23:10:03 +0300

    version

--- unitdata.pas
+++ unitdata.pas
@@ -7,7 +7,7 @@ uses
 
 const
   ApplicationName = 'Unbound Bible';
-  ApplicationVersion = '4.1';
+  ApplicationVersion = '5.0';
   BibleDirectory = 'bibles';
   TitleDirectory = 'titles';
   LangDirectory = 'localization';
