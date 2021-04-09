b717d5fc8454da9e98091de8620900fcc02e55d7
Vladimir Rybant <vrybant@gmail.com>
2021-04-09 19:10:34 +0300

    move and rename

---
+++ modules/zip.bat
@@ -0,0 +1,12 @@
+@echo off
+
+"C:\Program Files\7-Zip\7z.exe" a es.rvr1909.bbl.unbound.zip es.rvr1909.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a en.kjv.bbl.unbound.zip     en.kjv.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a en.kjv+.bbl.unbound.zip    en.kjv+.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a en.strong.dct.unbound.zip  en.strong.dct.unbound
+"C:\Program Files\7-Zip\7z.exe" a en.ob.xrefs.unbound.zip    en.ob.xrefs.unbound
+"C:\Program Files\7-Zip\7z.exe" a ru.rstw.bbl.unbound.zip    ru.rstw.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a ru.rst+.bbl.unbound.zip    ru.rst+.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a ru.ob.xrefs.unbound.zip    ru.ob.xrefs.unbound
+"C:\Program Files\7-Zip\7z.exe" a ru.strong.dct.unbound.zip  ru.strong.dct.unbound
+"C:\Program Files\7-Zip\7z.exe" a uk.ubio.bbl.unbound.zip    uk.ubio.bbl.unbound
