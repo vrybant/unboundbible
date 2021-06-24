91c6805a5a7064c2647f9a8ee449f104505a037b
Vladimir Rybant <vrybant@gmail.com>
2021-06-24 21:03:27 +0300

    rename

--- modules/zip.bat
+++ modules/zip.bat
@@ -1,12 +1,12 @@
 @echo off
 
-"C:\Program Files\7-Zip\7z.exe" a es.rvr1909.bbl.unbound.zip es.rvr1909.bbl.unbound
-"C:\Program Files\7-Zip\7z.exe" a en.kjv.bbl.unbound.zip     en.kjv.bbl.unbound
-"C:\Program Files\7-Zip\7z.exe" a en.kjv+.bbl.unbound.zip    en.kjv+.bbl.unbound
-"C:\Program Files\7-Zip\7z.exe" a en.strong.dct.unbound.zip  en.strong.dct.unbound
-"C:\Program Files\7-Zip\7z.exe" a en.ob.xrefs.unbound.zip    en.ob.xrefs.unbound
-"C:\Program Files\7-Zip\7z.exe" a ru.rstw.bbl.unbound.zip    ru.rstw.bbl.unbound
-"C:\Program Files\7-Zip\7z.exe" a ru.rst+.bbl.unbound.zip    ru.rst+.bbl.unbound
-"C:\Program Files\7-Zip\7z.exe" a ru.ob.xrefs.unbound.zip    ru.ob.xrefs.unbound
-"C:\Program Files\7-Zip\7z.exe" a ru.strong.dct.unbound.zip  ru.strong.dct.unbound
-"C:\Program Files\7-Zip\7z.exe" a uk.ubio.bbl.unbound.zip    uk.ubio.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a esrvr1909.bbl.unbound.zip esrvr1909.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a kjv.bbl.unbound.zip       kjv.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a kjv+.bbl.unbound.zip      kjv+.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a strong.dct.unbound.zip    strong.dct.unbound
+"C:\Program Files\7-Zip\7z.exe" a ob.xrefs.unbound.zip      ob.xrefs.unbound
+"C:\Program Files\7-Zip\7z.exe" a rstw.bbl.unbound.zip      rstw.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a rst+.bbl.unbound.zip      rst+.bbl.unbound
+"C:\Program Files\7-Zip\7z.exe" a obru.xrefs.unbound.zip    obru.xrefs.unbound
+"C:\Program Files\7-Zip\7z.exe" a strongru.dct.unbound.zip  strongru.dct.unbound
+"C:\Program Files\7-Zip\7z.exe" a ubio.bbl.unbound.zip      ubio.bbl.unbound
